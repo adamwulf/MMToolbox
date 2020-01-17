b0d5d40cf7ba037fa0e95d3ab0ca8f7fd3dc1813
Adam Wulf <adam.wulf@gmail.com>
2020-01-16 23:39:56 -0600

    Initial Commit

---
+++ MMToolbox/MMToolbox.h
@@ -0,0 +1,19 @@
+//
+//  MMToolbox.h
+//  MMToolbox
+//
+//  Created by Adam Wulf on 1/16/20.
+//  Copyright © 2020 Milestone Made. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MMToolbox.
+FOUNDATION_EXPORT double MMToolboxVersionNumber;
+
+//! Project version string for MMToolbox.
+FOUNDATION_EXPORT const unsigned char MMToolboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMToolbox/PublicHeader.h>
+
+
